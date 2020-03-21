84c54f3d4668b37ab46b9b10e3d6f961c82cd7f6
ubuntu <stephon.savic@gmail.com>
2020-03-21 18:06:27 +0100

    fixed tests

--- tests/test_typed_priority_array.py
+++ tests/test_typed_priority_array.py
@@ -21,55 +21,62 @@ valid_args_lists = [[0, 1, -1, 5, -5], [1, 2, 3], [0, 3, -1]]
 valid_args = valid_args_types + valid_args_lists
 invalid_args = [InvalidObject, object(), []]
 
-@pytest.mark.parametrize("test_input", valid_args)
-def test_constructor(test_input):
+@pytest.mark.parametrize("test_input", valid_args_types)
+def test_constructor_types(test_input):
     assert TypedPriorityArray(test_input) is not None
+
+@pytest.mark.parametrize("test_input", valid_args_lists)
+def test_constructor_lists(test_input):
+    assert TypedPriorityArray(*test_input) is not None
 
 @pytest.mark.parametrize("test_input", invalid_args)
 def test_failing_constructor(test_input):
     with pytest.raises(TypeError):
         TypedPriorityArray(test_input)
 
-@pytest.mark.parametrize("test_input", [x for x in valid_args if isinstance(x, list)])
+@pytest.mark.parametrize("test_input", valid_args_lists)
 def test_contains(test_input):
-    tpa = TypedPriorityArray(test_input)
+    tpa = TypedPriorityArray(*test_input)
     for x in test_input:
         assert x in tpa
 
 @pytest.mark.parametrize("test_input", valid_args)
 def test_length(test_input):
-    tpa = TypedPriorityArray(test_input)
     if isinstance(test_input, type):
+        tpa = TypedPriorityArray(test_input)
         assert tpa.length == 0
         assert len(tpa) == 0
     elif isinstance(test_input, list):
+        tpa = TypedPriorityArray(*test_input)
         assert tpa.length == len(test_input)
         assert len(tpa) == len(test_input)
 
 @pytest.mark.parametrize("test_input", valid_args)
 def test_array_type(test_input):
-    tpa = TypedPriorityArray(test_input)
     if isinstance(test_input, type):
+        tpa = TypedPriorityArray(test_input)
         assert tpa.array_type == test_input
     elif isinstance(test_input, list):
+        tpa = TypedPriorityArray(*test_input)
         assert tpa.array_type == type(test_input[0])
 
 @pytest.mark.parametrize("test_input", valid_args)
 def test_element_type(test_input):
-    tpa = TypedPriorityArray(test_input)
     if isinstance(test_input, type):
+        tpa = TypedPriorityArray(test_input)
         assert tpa.array_type == test_input
     elif isinstance(test_input, list):
+        tpa = TypedPriorityArray(*test_input)
         assert tpa.array_type == type(test_input[0])
 
-@pytest.mark.parametrize("test_input", [1, 4, 3, 5])
+@pytest.mark.parametrize("test_input", [[1, 4, 3, 5]])
 def test_str(test_input):
-    tpa = TypedPriorityArray(test_input)
+    tpa = TypedPriorityArray(*test_input)
     assert str(tpa) == "[1 <= 3 <= 4 <= 5]"
 
 @pytest.mark.parametrize("test_input", valid_args_lists)
 def test_reverse(test_input):
-    tpa = TypedPriorityArray(test_input, reverse=True)
+    tpa = TypedPriorityArray(*test_input, reversed=True)
 
     sorted_input = sorted(test_input, reverse=True)
     tpa_len = len(tpa)
@@ -78,7 +85,7 @@ def test_reverse(test_input):
 
 @pytest.mark.parametrize("test_input", valid_args_lists)
 def test_cmp(test_input):
-    tpa = TypedPriorityArray(test_input, cmp=lambda x, y: y - x)
+    tpa = TypedPriorityArray(*test_input, cmp=lambda x, y: y - x)
 
     sorted_input = sorted(test_input, reverse=True)
     tpa_len = len(tpa)
