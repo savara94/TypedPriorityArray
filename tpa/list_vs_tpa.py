5edab5b5dd5564c1370b700dbb9c4ccc5ca745de
ubuntu <stephon.savic@gmail.com>
2020-03-30 13:45:30 +0200

    changed structure, now all projects are all within their folders

--- tpa/list_vs_tpa.py
+++ tpa/list_vs_tpa.py
@@ -1,11 +1,11 @@
 from src.stefan.typed_priority_array import TypedPriorityArray
 import time
 
-maximum = 50000
+maximum = 70000
 data = range(maximum)
 
-#collection = list(data)
-collection = TypedPriorityArray(*data)
+collection = list(data)
+#collection = TypedPriorityArray(*data)
 times = []
 
 print("Started testing for {}".format(type(collection)))
@@ -14,7 +14,6 @@ for x in range(maximum + 1000):
     query_time = time.time()
     x in collection
     times.append(time.time() - query_time)
-
 
 print('Average time {}'.format(sum(times) / len(times)))
 print('Total time {}'.format(time.time() - start))
