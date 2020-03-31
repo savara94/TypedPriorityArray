c4502764eff5183260c0b986293299c70d27c484
ubuntu <stephon.savic@gmail.com>
2020-03-31 16:27:27 +0200

    fixed few tests, and added pop and insert

--- linked_list/tests/test_linked_list.py
+++ linked_list/tests/test_linked_list.py
@@ -16,7 +16,7 @@ LinkedList = module.LinkedList
 sys.argv.pop(1)
 pytest.main()
 
-valid_args = ['hello', [1, 2, 3], [object(), 'hello'], [(1, 2), (2, 3), (3, 4)]]
+valid_args = ['hello', [1, 2, 3], [object(), 'hello'], [(1, 2), (2, 3), (3, 4), (4, 5), (5, 6)]]
 invalid_args = [object(), 1]
 
 @pytest.mark.parametrize("test_input", valid_args)
@@ -28,7 +28,7 @@ def test_constructor_fail(test_input):
     with pytest.raises(ValueError):
         LinkedList(test_input)
 
-@pytest.mark.parametrize("test_input", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+@pytest.mark.parametrize("test_input", [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]])
 def test_constructor_fail_max_size(test_input):
     with pytest.raises(MemoryError):
         LinkedList(test_input, max_size=5)
@@ -71,9 +71,10 @@ def test_iterator(test_input):
 def test_append(test_input):
     ll = LinkedList(test_input)
 
-    for i in range(10):
-        ll.append(i)
-        test_input.append(i)
+    if 'append' in dir(test_input):
+        for i in range(10):
+            ll.append(i)
+            test_input.append(i)
 
     for i, x in enumerate(ll):
         assert x == test_input[i]
@@ -96,8 +97,7 @@ def test_not_contains():
     test_input = [1, 2, 3, 4]
     ll = LinkedList(test_input)
 
-    for x in test_input:
-        assert x not in ll
+    assert 5 not in  ll
 
 @pytest.mark.parametrize("test_input", valid_args)
 def test_reverse(test_input):
@@ -108,12 +108,32 @@ def test_reverse(test_input):
         assert x == test_input[length - i - 1]
 
 @pytest.mark.parametrize("test_input", valid_args)
+def test_pop(test_input):
+    if isinstance(test_input, list):        
+        ll = LinkedList(test_input)
+        
+        while len(test_input) > 0:
+            index = len(test_input) // 2
+            assert ll.pop(index) == test_input.pop(index)
+            assert list(iter(ll)) == test_input
+
+@pytest.mark.parametrize("test_input", valid_args)
+def test_insert(test_input):
+    if isinstance(test_input, list):        
+        ll = LinkedList(test_input)
+
+        indices = [len(test_input) - 1, len(test_input) // 2, 0]
+        
+        for index in indices:
+            ll.insert(index, index)
+            test_input.insert(index, index)
+            assert list(iter(ll)) == test_input
+
+@pytest.mark.parametrize("test_input", valid_args)
 def test_inverse(test_input):
-    ll = LinkedList(test_input)
-    length = len(test_input)
-    inversed = ll.inverse()
+    ll = LinkedList(test_input, max_size=100)
+    ll_i = ll.inverse()
+    assert isinstance(ll_i, LinkedList)
+    assert ll_i.max_size == ll.max_size
 
-    assert isinstance(inversed, LinkedList)
-
-    for i, x in enumerate(inversed):
-        assert x == test_input[length - i - 1]
+    assert list(ll_i) == list(reversed(test_input))
