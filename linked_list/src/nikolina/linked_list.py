101075c420b4a12367da7dcff1e56f60a380d84b
nina273 <lukanovic.nikolina@gmail.com>
2020-04-04 05:50:20 +0200

    nina

--- linked_list/src/nikolina/linked_list.py
+++ linked_list/src/nikolina/linked_list.py
@@ -1,3 +1,137 @@
+from collections.abc import Iterable
+class _Node:
+    '''Node object in Linked List'''
+    def __init__(self, data):
+        self.item = data
+        self.next = None
 class LinkedList:
+    '''Linked List implementation'''
+    def __init__(self, *args, **kwargs):
+        if len(args) == 0:
+            raise ValueError("List cann't be empty!")
+        self.__head = None
+        self.__max_size = kwargs["max_size"] if "max_size" in kwargs else None
+        if len(args) == 1:
+            if not isinstance(args[0], Iterable):
+                raise ValueError
+            i = 0
+            if self.__max_size != None and len(args[0]) > self.__max_size:
+                raise MemoryError
+            if len(args[0]) == 1:
+                self.insert(0, args[0])
+            for item in args[0]:
+                self.insert(i, item)
+                i += 1
+        else:
+            if self.__max_size != None and len(args) > self.__max_size:
+                raise MemoryError
+            i = 0
+            for _arg in args:
+                self.insert(i, _arg)
+                i += 1
+    def insert(self, index, element):
+        if index == 0:
+            __new_node = _Node(element)
+            __new_node.next = self.__head
+            self.__head = __new_node
+            return
+        current = self.__head
+        i = 1
+        while current is not None and i <= index - 1:
+            current = current.next
+            i += 1
+        if current == None:
+            raise IndexError
+        else:
+            __new_node = _Node(element)
+            __new_node.next = current.next
+            current.next = __new_node
+        if index == len(self):
+            temp = self.__head
+            while temp.next is not None:
+                temp = temp.next
+            temp.next = _Node(element)
+            return
+    def pop(self, index):
+        if self.__head == None:
+            return
+        temp = self.__head
+        if index == 0:
+            prev = self.__head
+            self.__head = temp.next
+            temp = None
+            return prev.item
+        for _i in range(index - 1):
+            temp = temp.next
+            if temp is None:
+                break
+        if temp is None:
+            return
+        if temp.next is None:
+            return
+        prev = temp.next
+        next = temp.next.next
+        temp.next = None
+        temp.next = next
+        return prev.item
+
+    def append(self, element):
+        self.insert(len(self), element)
+
+    def index_of(self, element):
+        __index = 0
+        current = self.__head
+        while current is not None:
+            if current.item == element:
+                return __index
+            current = current.next
+            __index += 1
+    def __iter__(self):
+        self.indexIter = -1
+        return self
+    def __next__(self):
+        if self.indexIter >= len(self) - 1:
+            raise StopIteration
+        self.indexIter += 1
+        return self[self.indexIter]
+    def __len__(self):
+        if self.__head is None:
+            return 0
+        current = self.__head.next
+        length = 0
+        while current is not None:
+            length += 1
+            current = current.next
+        return length
+    def __contains__(self, element):
+        current = self.__head
+        while current is not None:
+            if current.item == element:
+                return True
+            current = current.next
+        return False
+    def __getitem__(self, index):
+        i = 0
+        current = self.__head
+        while current.next is not None:
+            if i == index:
+                return current.item
+            current = current.next
+            i += 1
+    def __str__(self):
+        ll = ""
+        current = self.__head
+        while current.next is not None:
+            if current.next.next != None:
+                ll += str(current.item) + ' -> '
+            else:
+                ll += str(current.item)
+            current = current.next
+        return ll
+    @property
+    def length(self):
+        return len(self)
+    @property
+    def max_size(self):
+        return self.__max_size
     
-    pass
